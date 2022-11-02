bdf1a8ff8b20d09c24e309cff59e32b97be1b870
unknown <karthik93460@gmail.com>
2022-11-02 20:14:36 +0530

    php code

---
+++ today_records.php
@@ -0,0 +1,70 @@
+<html>
+<head>
+<title>view data on database</title>
+</head>
+<body>
+
+<table border="2">
+<tr>
+<th>sno</th>
+<th>regno</th>
+<th>name</th>
+<th>gender</th>
+<th>course</th>
+<th>book</th>
+<th>author</th>
+<th>phone number</th>
+<th>email</th>
+<th>Date</th>
+</tr>
+
+<?php
+  
+$s="localhost";
+$u="root";
+$p="";
+$db="library";
+$conn=mysqli_connect($s,$u,$p,$db);
+  
+  // GET CONNECTION ERRORS
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+  
+  // SQL QUERY
+  $query = "SELECT * FROM lib where date=curdate();";
+  
+  // FETCHING DATA FROM DATABASE
+  $result = $conn->query($query);
+  
+    if ($result->num_rows > 0) 
+    {
+        // OUTPUT DATA OF EACH ROW
+        while($row = $result->fetch_assoc())
+        {
+            echo "<tr>
+		<td>".$row['sno']."</td>
+		<td>".$row['regno']."</td>
+		<td>".$row['name']."</td>
+		<td>".$row['gender']."</td>
+		<td>".$row['course']."</td>
+		<td>".$row['book']."</td>
+		<td>".$row['author']."</td>
+		<td>".$row['phone']."</td>
+		<td>".$row['email']."</td>
+		<td>".$row['date']."</td>
+		
+		</tr>
+		";
+        }
+    } 
+    else {
+        echo "<h5 style='color:red'>Today no records registered</h5>";
+    }
+  
+   $conn->close();
+  
+?>
+</table>
+</body>
+</html>
