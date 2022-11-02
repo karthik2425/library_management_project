2f9d4f6157b445e4cb88aada8ab391a3d63c5aba
unknown <karthik93460@gmail.com>
2022-11-02 20:13:54 +0530

    php code

---
+++ registration.php
@@ -0,0 +1,51 @@
+
+<!Document>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>library registration</title>
+<style>
+body{
+background: rgb(2,0,36);
+background: linear-gradient(120deg,#2980b9,#8e44ad);
+}
+</style>
+
+
+</head>
+<body>
+ 
+   <center>
+   <bold>
+
+    <h1 style=color:blue><u>LIBRARY MANAGEMENT </u></h1>
+     
+     <form method="POST" action="connect.php">
+
+<h3><table><tr><td>Regd No</td><td><input type="text" name="regno"></td></tr>
+<tr><td>Name</td><td><input type="text" name="name"></td></tr>
+<tr><td>Gender</td><td><input  type="radio"  name="gender"  value="Male">Male
+ <input  type="radio"  name="gender"  value="Female">Female 
+</tr>
+<tr><td>Course</td><td><input type="text" name="course">
+</tr>
+
+<tr><td>Book name</td><td><input type="text" name="book"</td></tr>
+<tr><td>Author</td><td><input type="text" name="author"</td></tr>
+
+<tr><td>Phone Number</td><td><input type="number" name="phone"</td></tr>
+<tr><td>Email id</td><td><input type="text" name="email"</td></tr>
+ <h2 style=>
+ <tr><td><input type="submit" value="submit" name="submit"></td></h2>
+ <td><input type="reset" value="clear" name="clear"></td></tr>
+ 
+ 
+ 
+ </bold>
+</table>
+</form>
+<a href="file:///C:/xampp/htdocs/project/home.html">HOME PAGE</a>
+</center>
+</body>
+</html>
